ca21297bc1a9f25fd635e77a0bc5d64e7d3df28f
saiful10400 <saifulislam10400i5@gmail.com>
2023-08-18 10:14:26 +0600

    minor change in path and file name.

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ document.getElementById("loginBtn").addEventListener("click",function(){
     let userPassword=document.getElementById("userPassword").value
     
     if(userEmail == "test@gmail.com" && userPassword == "admin"){
-        window.location.href="../html/bank.html"
+        window.location.href="../html/index.html"
     }
     else{
         alert("Invalid email or password.")
