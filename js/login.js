ede9f60d5ee9aafa76673c9baadd83904b9e4b56
saiful10400 <saifulislam10400i5@gmail.com>
2023-08-18 10:24:56 +0600

    emin

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ document.getElementById("loginBtn").addEventListener("click",function(){
     let userPassword=document.getElementById("userPassword").value
     
     if(userEmail == "test@gmail.com" && userPassword == "admin"){
-        window.location.href="../html/index.html"
+        window.location.href="..baper-bank-reloaded/html/index.html"
     }
     else{
         alert("Invalid email or password.")
